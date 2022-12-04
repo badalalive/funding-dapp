9f689b33c4745ca6448329db6abf1ba713e1b0ae
badal gupta <badal965484@gmail.com>
2022-12-04 11:33:23 +0530

    web3 library and truffle library

--- contracts/Demo.sol
+++ contracts/Demo.sol
@@ -5,8 +5,25 @@ contract Demo {
     // address of owner
     address public owner;
 
+    // no of funders
+    uint256 public noOfFunder;
+
+    mapping (uint256 => address) funder;
+
     constructor() {
         owner = msg.sender;
     }
+
+    receive() external payable {}
+
+    function transfer() external payable {
+        funder[noOfFunder++] = msg.sender;
+    }
+
+    function withdraw(uint256 _withdrawAmount) external {
+        require(_withdrawAmount <= 2e18,"Can not withdraw more than 2 ether");
+        payable(msg.sender).transfer(_withdrawAmount);
+    }
+
 
 }
